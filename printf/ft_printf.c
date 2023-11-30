7c809b65e8ed2f4772b23945ba0e5bef755160d4
Sougon <guillaume.humm@yahoo.fr>
2023-11-30 14:00:35 +0100

    libft

---
+++ printf/ft_printf.c
@@ -0,0 +1,8 @@
+
+#include "../Libft/libft.h"
+#include "ft_printf.h"
+
+int	ft_printf(const char *, ...)
+{
+	
+}
---
+++ printf/ft_printf.h
@@ -0,0 +1,8 @@
+
+#ifndef PRINTF_H
+# define PRINTF_H
+
+int	ft_printf(const char *, ...);
+
+
+#endif
