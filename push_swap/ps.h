4829e15c67e1000a09bc38b17606704181a4938d
Sougon <guillaume.humm@yahoo.fr>
2024-01-03 14:54:01 +0100

    maj

--- push_swap/ps.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef PS_H
-# define PS_H
-
-
-
-#endif
---
+++ push_swap/src/push_swap.c
@@ -0,0 +1,33 @@
+
+#include "ps.h"
+
+int push_swap()
+{
+
+}
+
+int main(int argc, char *argv[])
+{
+    int taille_pile;
+    int pile_a[];
+    int pile_b[];
+    int *ptr_pile_a;
+    char    **ptr_argv;
+
+    if (argc < 2)
+    {
+        ft_printf("ERREUR : Pas assez d'arguments\n");
+        return (1);
+    }   
+    taille_pile = argc - 1;
+    pile_a[taille_pile];
+    pile_b[taille_pile];
+    ptr_pile_a = pile_a;
+    ptr_argv = argv + 1;
+    while (*ptr_argv)
+    {
+        *ptr_pile_a++ = ft_atoi(*ptr_argv++)
+    }
+    push_swap(int argc)
+    
+}
