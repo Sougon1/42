f8e040a61ffb43168ea91ba993840a75f167cdb9
Sougon <guillaume.humm@yahoo.fr>
2024-01-18 16:00:21 +0100

    m

--- push_swap/Libft/libft.h
+++ push_swap/Libft/libft.h
@@ -41,7 +41,7 @@ char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char	**ft_split(char const *s, char c);
 
-int		ft_lstsize(t_list *lst);
+//int		ft_lstsize(t_list *lst);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
 int		ft_atoi(const char *nptr);
@@ -57,11 +57,11 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 //size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 //size_t	ft_strlen(const char *a);
 
-void	ft_lstiter(t_list *lst, void (*f)(void*));
-void	ft_lstclear(t_list **lst, void (*del)(void*));
-void	ft_lstdelone(t_list *lst, void (*del)(void*));
-void	ft_lstadd_back(t_list **lst, t_list *new);
-void	ft_lstadd_front(t_list **lst, t_list *new);
+//void	ft_lstiter(t_list *lst, void (*f)(void*));
+//void	ft_lstclear(t_list **lst, void (*del)(void*));
+//void	ft_lstdelone(t_list *lst, void (*del)(void*));
+//void	ft_lstadd_back(t_list **lst, t_list *new);
+//void	ft_lstadd_front(t_list **lst, t_list *new);
 void	ft_putnbr_fd(int n, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putstr_fd(char *s, int fd);
