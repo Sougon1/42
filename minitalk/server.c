9fdba6f9b5aac3737441be212f230a28ac94a066
Sougon1 <guillaume.humm@yahoo.fr>
2024-05-28 15:49:48 +0200

    maj

--- minitalk/server.c
+++ minitalk/server.c
@@ -6,10 +6,27 @@
 /*   By: ghumm <ghumm@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 11:40:27 by ghumm             #+#    #+#             */
-/*   Updated: 2024/05/28 12:53:07 by ghumm            ###   ########.fr       */
+/*   Updated: 2024/05/29 11:06:43 by ghumm            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
 
+
+
+int main()
+{
+    pid_t pid = getpid();
+    ft_printf("Server running with PID: %d\n", pid);
+
+    signal(SIGUSR1, client);
+    signal(SIGUSR2, server);
+
+    while (1)
+    {
+        pause();
+    }
+
+    return (0);
+}
