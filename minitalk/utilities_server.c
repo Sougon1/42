d8efaf1e119591e0b81d94ed7b7602cf17233174
Sougon1 <guillaume.humm@yahoo.fr>
2024-06-05 12:03:11 +0200

    maj

--- minitalk/utilities_server.c
+++ minitalk/utilities_server.c
@@ -6,7 +6,7 @@
 /*   By: ghumm <ghumm@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 11:28:13 by ghumm             #+#    #+#             */
-/*   Updated: 2024/06/05 11:28:35 by ghumm            ###   ########.fr       */
+/*   Updated: 2024/06/05 12:02:55 by ghumm            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,19 @@
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	unsigned char		*d;
-	const unsigned char	*s = (const unsigned char *)src;
+	char		*dest1;
+	const char	*src1;
+	size_t		i;
 
-	d = (unsigned char *)dest;
-	while (n--)
+	if (dest == NULL && src == NULL && n > 0)
+		return (NULL);
+	i = 0;
+	dest1 = (char *) dest;
+	src1 = (const char *) src;
+	while (i < n)
 	{
-		*d++ = *s++;
+		dest1[i] = src1[i];
+		i++;
 	}
 	return (dest);
 }
