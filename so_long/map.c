f3f1021883fffd700b4cb63847158b6e89f92e04
sougon <guillaume.humm@yahoo.fr>
2024-06-14 17:19:55 +0200

    maj

--- so_long/map.c
+++ so_long/map.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   map.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ghumm <ghumm@student.42.fr>                +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 17:18:33 by ghumm             #+#    #+#             */
-/*   Updated: 2024/06/11 17:37:56 by ghumm            ###   ########.fr       */
+/*   Updated: 2024/06/14 16:59:46 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,76 +47,49 @@
 //     // security_map(map);
 // }
 
-void print_visible_whitespace(const char str)
-{
-        switch (str)
-        {
-            case '\n':
-                printf("\\n\n");
-                break;
-            case '\t':
-                printf("\\t");
-                break;
-            case '\r':
-                printf("\\r");
-                break;
-            case '\f':
-                printf("\\f");
-                break;
-            case '\v':
-                printf("\\v");
-                break;
-            case ' ':
-                printf("\\s");
-                break;
-            case '\0':
-                printf("\\0");
-                break;
-            default:
-                putchar(str);
-                break;
-        }
+#include "so_long.h"
+
+void print_visible_whitespace(const char str) {
+    switch (str) {
+        case '\n': printf("\\n\n"); break;
+        case '\t': printf("\\t"); break;
+        case '\r': printf("\\r"); break;
+        case '\f': printf("\\f"); break;
+        case '\v': printf("\\v"); break;
+        case ' ': printf("\\s"); break;
+        case '\0': printf("\\0"); break;
+        default: putchar(str); break;
+    }
 }
 
-void traiter_buffer(const char *buffer, ssize_t bytes_read, t_map *map, t_analyse *analyse)
-{
-    ssize_t i;
+void traiter_buffer(const char *buffer, ssize_t bytes_read, t_map *map, t_analyse *analyse) {
+    ssize_t i = 0;
 
-    i = 0;
-    while (i < bytes_read)
-    {
-        if (buffer[i] == '\n')
-        {
-            map->carte[analyse->ligne_count][analyse->largeur_actuelle] = '\0'; // Remplace '\n' par '\0'
+    while (i < bytes_read) {
+        if (buffer[i] == '\n') {
+            map->carte[analyse->ligne_count][analyse->largeur_actuelle] = '\0';
             analyse->ligne_count++;
             if (analyse->largeur_actuelle > analyse->largeur_max)
                 analyse->largeur_max = analyse->largeur_actuelle;
             analyse->largeur_actuelle = 0;
-            if (analyse->prem_ligne)
-            {
+            if (analyse->prem_ligne) {
                 map->largeur = analyse->largeur_max - 1;
                 analyse->prem_ligne = 0;
             }
-        }
-        else
-        {
+        } else {
             map->carte[analyse->ligne_count][analyse->largeur_actuelle] = buffer[i];
             analyse->largeur_actuelle++;
         }
-        print_visible_whitespace(buffer[i]);        // Afficher le caractère lu
-        // printf("%c", buffer[i]);
+        print_visible_whitespace(buffer[i]);
         i++;
     }
     map->carte[analyse->ligne_count][analyse->largeur_actuelle] = '\0';
 }
 
-
-
-
 void compter_lignes_et_colonnes(int fd, t_map *map) {
     ssize_t bytes_read;
     char buffer[BUFFER_SIZE];
-    t_analyse analyse = {0, 1, 0, 0};  // Initialiser les variables d'analyse
+    t_analyse analyse = {0, 1, 0, 0};
 
     while ((bytes_read = read(fd, buffer, BUFFER_SIZE)) > 0) {
         traiter_buffer(buffer, bytes_read, map, &analyse);
@@ -125,20 +98,18 @@ void compter_lignes_et_colonnes(int fd, t_map *map) {
     printf("\n");
     map->hauteur = analyse.ligne_count + 1;
 
-      int o = 0;
-      int j = 0;
-            printf("\n");
-        while (j < map->hauteur)
-        {
-            o = 0;
-            while (o < map->largeur + 1)
-            {
-                print_visible_whitespace(map->carte[j][o++]);
-            }
-            printf("\n");
-            j++;
+    int o = 0;
+    int j = 0;
+    printf("\n");
+    while (j < map->hauteur) {
+        o = 0;
+        while (o < map->largeur + 1) {
+            print_visible_whitespace(map->carte[j][o++]);
         }
-                    printf("\n");
+        printf("\n");
+        j++;
+    }
+    printf("\n");
 
     printf("\ntaille largeur = %d\ntaille hauteur = %d\n", map->largeur, map->hauteur);
 
@@ -147,7 +118,6 @@ void compter_lignes_et_colonnes(int fd, t_map *map) {
         exit(EXIT_FAILURE);
     }
 }
-
 
 void lire_carte(const char *nom_fichier, t_map *map) {
     int fd;
@@ -164,21 +134,20 @@ void lire_carte(const char *nom_fichier, t_map *map) {
         perror("Erreur lors de la fermeture du fichier");
         exit(EXIT_FAILURE);
     }
-    security_map(map);
+    // security_map(map); // Assurez-vous d'appeler cette fonction si nécessaire
 }
 
-
-
-
-
-
-void creer_fenetre(t_map *map)
-{
+void creer_fenetre(t_map *map) {
     map->graphics.mlx = mlx_init();
-    int largeur_fenetre = map->largeur * TAILLE_CASE;  // Largeur de la fenêtre basée sur la carte
-    int hauteur_fenetre = map->hauteur * TAILLE_CASE;  // Hauteur de la fenêtre basée sur la carte
-
-    map->graphics.fenetre = mlx_new_window(map->graphics.mlx, largeur_fenetre, hauteur_fenetre, "Ma Carte");
+    if (map->graphics.mlx == NULL) {
+        ft_printf("Erreur lors de l'initialisation de la connexion à X\n");
+        exit(1);
+    }
+    map->graphics.fenetre = mlx_new_window(map->graphics.mlx, map->largeur * TAILLE_CASE, map->hauteur * TAILLE_CASE, "Ma Carte");
+    if (map->graphics.fenetre == NULL) {
+        ft_printf("Erreur lors de la création de la fenêtre\n");
+        exit(1);
+    }
 }
 
 void dessiner_case(t_map *map, int x, int y, int couleur) {
@@ -195,177 +164,26 @@ void dessiner_case(t_map *map, int x, int y, int couleur) {
 }
 
 void dessiner_carte_rec(t_map *map, int x, int y) {
-    // if (x >= map->largeur) {
-    //     x = 0;
-    //     y++; // Passer à la ligne suivante
-    // }
-
-    // if (y >= map->hauteur) {
-    //     return;
-    // }
-
-    int couleur = 0xFFFF90; // Blanc par défaut
+    int couleur = 0xFFFF90;
 
     char cell = map->carte[y][x];
-    // printf("Case (%d, %d): Cellule = %c\n", x, y, cell); // Instruction de débogage
     switch (cell) {
-        case '1':
-            couleur = 0x009FF0; // Noir pour les murs
-            break;
-        case 'P':
-            couleur = 0xFF0000; // Rouge pour le joueur
-            break;
-        case 'C':
-            couleur = 0x00FF00; // Vert pour les collectables
-            break;
-        case 'E':
-            couleur = 0x0000FF; // Bleu pour la sortie
-            break;
-        case '0':
-            couleur = 0xFFFFFF; // Blanc pour les espaces vides
-            break;
-        // Ajoutez d'autres cas pour d'autres éléments de la carte si nécessaire
+        case '1': couleur = 0x009FF0; break;
+        case 'P': couleur = 0xFF0000; break;
+        case 'C': couleur = 0x00FF00; break;
+        case 'E': couleur = 0x0000FF; break;
+        case '0': couleur = 0xFFFFFF; break;
     }
 
-    // printf("Case (%d, %d): Couleur = %06X\n", x, y, couleur); // Instruction de débogage
     dessiner_case(map, x, y, couleur);
-       // Vérifiez si vous devez passer à la case suivante ou à la ligne suivante
-    if (x < map->largeur - 1) { // Vérifiez si vous êtes à la fin de la ligne
-        dessiner_carte_rec(map, x + 1, y); // Passez à la case suivante sur la même ligne
-    } else if (y < map->hauteur - 1) { // Vérifiez si vous êtes à la fin de la carte
-        dessiner_carte_rec(map, 0, y + 1); // Passez à la première case de la ligne suivante
+
+    if (x < map->largeur - 1) {
+        dessiner_carte_rec(map, x + 1, y);
+    } else if (y < map->hauteur - 1) {
+        dessiner_carte_rec(map, 0, y + 1);
     }
-    
-    
-    
-    // dessiner_carte_rec(map, x + 1, y); // Appel récursif pour passer à la case suivante sur la même ligne
-    // Ajouter un appel récursif pour passer à la ligne suivante
-    // dessiner_carte_rec(map, x, y + 1);
 }
-
-
-
-
 
 void dessiner_carte(t_map *map) {
     dessiner_carte_rec(map, 0, 0);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// void lire_ligne(FILE *fichier, char carte[MAX_ROWS][MAX_COLS], int row, int hauteur)
-// {
-//     if (row >= hauteur)
-//         return;
-    
-//     fgets(carte[row], MAX_COLS, fichier);
-//     lire_ligne(fichier, carte, row + 1, hauteur);
-// }
-
-// void lire_carte(const char *nom_fichier, t_map *map)
-// {
-//     FILE *fichier = fopen(nom_fichier, "r");
-//     if (fichier == NULL) {
-//         perror("Erreur lors de l'ouverture du fichier");
-//         exit(1);
-//     }
-
-//     fscanf(fichier, "%d %d\n", &map->largeur, &map->hauteur);
-//     lire_ligne(fichier, map->carte, 0, map->hauteur);
-
-//     fclose(fichier);
-// }
-
-// void creer_fenetre(t_map *map)
-// {
-//     map->mlx = mlx_init();
-//     map->fenetre = mlx_new_window(map->mlx, map->largeur * TAILLE_CASE, map->hauteur * TAILLE_CASE, "Ma Carte");
-
-//     // Gestion des événements clavier
-//     mlx_key_hook(map->fenetre, key_hook, NULL);
-//     printf("Nombre de touches pressées : 0 ");
-//     mlx_hook(map->fenetre, 17, 0, close_window, NULL);
-
-//     // Boucle principale
-//     mlx_loop(map->mlx);
-// }
-
-
-// void dessiner_carte_rec(t_map *map, int x, int y)
-// {
-//     if (x >= map->largeur) {
-//         x = 0;
-//         y++;
-//     }
-
-//     if (y >= map->hauteur) {
-//         return;
-//     }
-
-//     int couleur = 0xFFFFFF; // Blanc par défaut
-
-//     switch (map->carte[y][x]) {
-//         case '1':
-//             couleur = 0x000000; // Noir pour les murs
-//             break;
-//         case 'P':
-//             couleur = 0xFF0000; // Rouge pour le joueur
-//             break;
-//         case 'C':
-//             couleur = 0x00FF00; // Vert pour les collectables
-//             break;
-//         case 'E':
-//             couleur = 0x0000FF; // Bleu pour la sortie
-//             break;
-//         case '0':
-//             couleur = 0xFFFFFF; // Blanc pour les espaces vides
-//             break;
-//         // Ajoutez d'autres cas pour d'autres éléments de la carte si nécessaire
-//     }
-
-//     mlx_pixel_put(map->mlx, map->fenetre, x * TAILLE_CASE, y * TAILLE_CASE, couleur);
-//     dessiner_carte_rec(map, x + 1, y);
-// }
-
-// void dessiner_carte(t_map *map)
-// {
-//     dessiner_carte_rec(map, 0, 0);
-// }
